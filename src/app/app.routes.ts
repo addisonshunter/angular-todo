234d53da3d12c239f9a6fe03dde586a86ce8f456
Terepy <avianpeacock@gmail.com>
2024-11-01 02:44:04 -0400

    Reformatting remaining files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { ToDo } from './todo/todo.component';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'todo' },
-    { path: 'todo', component: ToDo },
+  { path: '', pathMatch: 'full', redirectTo: 'todo' },
+  { path: 'todo', component: ToDo },
 ];
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,11 +2,11 @@ import { Component } from "@angular/core";
 import { TodoListComponent } from "./todo-list/todo-list.component";
 
 @Component({
-    standalone: true,
-    selector: 'atd-todo',
-    templateUrl: './todo.component.html',
-    styleUrl: './todo.component.css',
-    imports: [ TodoListComponent ],
+  standalone: true,
+  selector: 'atd-todo',
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css',
+  imports: [ TodoListComponent ],
 })
 export class ToDo {
     
